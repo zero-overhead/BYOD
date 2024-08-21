7377f50e6f441248e9ac598ef114b201626a6343
rcmlz <19784049+rcmlz@users.noreply.github.com>
2024-08-21 16:08:52 +0200

    switching to VM only

--- README.md
+++ README.md
@@ -1,75 +1,80 @@
-# BYOD + VirtualBox + NixOS + home-manager
-BYOD $\to$ heterogene Umgebung + (VirtualBox + NixOS + home-manager) $\to$ indentische Umgebung fürs Programmieren im Klassenraum.
+# BYOD + VirtualBox/UTM + NixOS = Unified Programming Environment
 
-Für Windows-, Mac- oder Linux-Rechner aber ~~iPad~~.
+## Aktualisierung
+Falls du schon eine NixOS-VM besitzt, und diese aktualisieren möchtest, führe ff. Befehle in einem Terminal von innerhalb der VM aus.
 
-## Vor dem Unterricht
-1. Starte deinen Computer, dann VirtualBox und dann die NixOS-VM
-2. Neue Unterrichtsmateralien zum ThemaXYZ holen:
-```bash
-cd ~/Documents
-git clone https://github.com/zero-overhead/ThemaXYZ
-```
-3. Bestehende Unterrichtsmateralien zum ThemaXYZ aktualisieren:
-```bash
-cd ~/Documents/ThemaXYZ
-git pull
-```
-Falls es dabei zu Fehlern kommt und du nicht mehr weiter weisst: Änderungen speichern, Backup anlegen (so geht nichts verloren) und main-Branch auf Ausgangszustand zurücksetzen:
-```bash
-git commit -am"Notfallsicherung1" # alle Änderungen speichern
-git branch Notfallsicherung1 # neuen Branch als Sicherungskopie anlegen
-git checkout main # auf main Branch wechseln
-git fetch --all
-git reset --hard origin/main
-```
-4. Je nach Bedarf TigerJython, Thonny, Jupyter, Filius, VS-Code etc. starten
-
-## Zum Ende des Unterrichts
-1. Alle bearbeiteten Dateien speichern
-2. VM sichern und VirtualBox beenden
-
-## Einmaliges Setup
-1. BYOD - du hast einen Windows-, Mac- oder Linux-Rechner aber ~~iPad~~ ?
-2. Installiere [VirtualBox](https://www.virtualbox.org/wiki/Downloads) auf deinem Rechner
-3. Lade das [NixOS-VirtualBox](https://nixos.org/download/#nixos-virtualbox)-Image herunter
-4. Starte VirtualBox und importiere das gerade heruntergeladene NixOS-VirtualBox-Image und
-5. setze der Virtuellen Maschine (VM) den RAM auf 4GB und die Prozessoren auf 2 oder 3
-6. Starte VM, du wirst automatisch eingelogged
-7. Setze Tastatur-Layout und Zeitzone (Settings -> Keyboard und Settings -> Timezone)
-8. Öffne eine Konsole (Kommandozeile, Terminal) und installiere [Home-Manager Stand Alone](https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone) durch ff. Befehle:
-
-```bash
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
-
-nix-channel --update
-```
-
-Ausloggen und wieder Einloggen. Dann
-
-```bash
-nix-shell '<home-manager>' -A install
-```
-9. Clone das Git-Repository mit der Systemkonfiguration für den Informatikunterricht
-```bash
-nix-shell -p git
-```
-dann
 ```bash
 git clone https://github.com/zero-overhead/BYOD
-mv ~/.config/home-manager ~/.config/home-manager-backup
-mv BYOD ~/.config/home-manager
-mkdir ~/.config/nix
-cp ~/.config/home-manager/nix.conf ~/.config/nix/
+
+cd BYOD
 ```
-10. Erstelle die Unterrichtsumgebung
+
+### VirtualBox
 ```bash
-home-manager switch
+cp configuration_VirtualBox.nix /etc/nixos/configuration.nix
 ```
-11. Ausloggen und wieder einloggen (User "demo" und Passwort "demo") und dann überprüfen, ob benötigte Software installiert ist.
-    - TigerJython
-    - Filius
-    - Jupyter
-    - Thonny
-    - Visual Studio Code
-12. Wenn alles i.O. ist, lege einen Sicherungspunkt für diese VM in VirtualBox an - so kannst du, wenn mal etwas schief gegangen ist, jederzeit auf diesen sauberen Zustand zurückwechseln.
+
+### UTM
+
+```bash
+cp configuration_UTM.nix /etc/nixos/configuration.nix
+```
+
+## Erstinstallation
+
+### Vorbereitung
+
+Windows: öffne den Datei-Explorer, klicke im Menü auf "Datei -> Ordner und Suchoptionen ändern -> Ansicht" und entferne den Haken bei "Erweiterung bei bekannten Dateitypen ausblenden". Klicke dann "Für Ordner übernehmen" 
+
+Mac: öffne den Finder, klicke dann im Menü "Einstellungen -> Erweitert" und aktiviere den Haken bei "Alle Dateinamensuffixe einblenden"
+
+### Windows/Mac mit Intel- oder AMD-CPU
+
+- Installiere VirtualBox. Du findest den Download unter https://www.virtualbox.org/
+- Kopiere die Datei ```Programmieren.ova``` im Ordner ```SuS-Setup``` vom USB-Stick auf deinen Computer nach bspw. ```Downloads```.
+- Öffne die Datei ```Programmieren.ova``` aus dem Ordner ```Downloads``` in VirtualBox.
+- Starte die VM
+- Nutzer/Passwort: demo/demo
+- Mit der rechten CTRL+F wechselst du ins Vollbild und wieder zurück. Alternativ ziehe die Maus an den unteren Bildschirmrand, dort siehst du einen Knopf ```_``` bzw. ```x``` zum verlassen der VM. Oder ziehe mit drei Fingern auf deinem Touchpad aufwärts oder abwärts.
+
+### Mac mit Apple-CPU (M1/M2/M3)
+
+- Installieren von UTM. Du findest den Download unter https://mac.getutm.app/
+- Kopiere die Datei ```Programmieren.utm``` im Ordner ```SuS-Setup-Mac``` vom USB-Stick auf deinen Computer nach bspw. ```Downloads```.
+- Öffne die Datei ```Programmieren.utm``` im Ordner ```Downloads``` in UTM unter "Neue Virtuelle Maschine erstellen -> öffnen" und folge den Anweisungen.
+- Starte die VM
+- Nutzer/Passwort: demo/demo
+- Lösche die Datei ```Programmieren.utm``` bis zum Ende des Schuljahres nicht aus dem Download-Ordner. 
+
+
+### Windows ARM-CPU (Snapdragon)
+siehe "Bei mir funktioniert das nicht ..." am Ende dieses Dokuments
+
+### Bei mir funktioniert das nicht ...
+
+Falls das Setup auf deinem Computer nicht gelingt:
+
+- installiere die Anwendungen direkt
+    - https://tigerjython.ch/de/products/download
+    - https://thonny.org/
+    - https://www.lernsoftware-filius.de/
+
+**oder**
+
+- nutze fürs Programmieren einen (älteren) Laptop, den du bei Freunden oder Familie für das Schuljahr ausleihst. Aktuell werden viele (an sich tadellose) Laptops ausgemustert, da sie die Hardwareanforderungen für Windows 11 nicht erfüllen. 
+
+**oder**
+
+- verwende für die Programmierübungen die Online-Lösungen
+    - https://webtigerjython.ethz.ch/ oder
+    - https://webtigerpython.ethz.ch/ oder
+    - https://sagecell.sagemath.org/ bzw. https://cocalc.com/
+    - ...
+
+Die Jupyter-Notebooks kannst du in dem Fall auf [Github](https://github.com/zero-overhead/sek2.inf) anschauen und bspw. mit [Binder](https://mybinder.org/v2/gh/rcmlz/edu-binder-env/python?urlpath=git-pull%3Frepo%3Dhttps%253A%252F%252Fgithub.com%252Fzero-overhead%252Fsek2.inf%26urlpath%3Dlab%252Ftree%252Fsek2.inf) oder [Colab](https://colab.research.google.com/) die Übungen bearbeiten.
+
+**Aber**: Support durch deine Informatik-Lehrperson wird nur für das Standard-Programmier-Setup via VM geleistet.
+
+### Aufräumen
+
+Nach Abschluss des Schuljahres versetzt du deinen Computer wieder in den Ausgangszustand, indem du die VMs in VirtualBox bzw. UTM und danach die Programme VirtualBox bzw. UTM selbst löschst.
---
+++ configuration_UTM.nix
@@ -0,0 +1,246 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Time Syncing and scripting
+  services.qemuGuest.enable = true;
+  # Clipboard Sharing
+  services.spice-vdagentd.enable = true;
+  # VirtFS alternative for directory sharing
+  services.spice-webdavd.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "de_CH.UTF-8";
+  
+  # Configure console keymap - Swiss
+  console.keyMap = "sg";
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "ch";
+      variant = "de_mac";
+    };
+  };
+
+  # https://nixos.wiki/wiki/KDE
+  # copy/paste from host needs spice-vdagend which does not work on wayland yet
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;
+  };
+  services.desktopManager.plasma6.enable = true; 
+ 
+  # Enable automatic login for the user.
+  services.displayManager = {
+    autoLogin.enable = true;
+    autoLogin.user = "demo";
+    defaultSession = "plasmax11";
+  };
+  
+  # settings saved for some applications (gtk3 applications, firefox)
+  programs.dconf.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.demo = {
+    isNormalUser = true;
+    description = "demo";
+    extraGroups = [ "networkmanager" "wheel" ];
+    # mkpasswd -m sha-512
+    hashedPassword ="$6$mJ1Nxf5ZJbTz.Ze7$syodN3YdfjtsB9vajYXGEoiuvS0vPv5R58LkzwV6qVVF5MjEZ/gHCMMYHb0GibcP./tW20EKCGMWM3N25FhtG1";
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    spice-vdagent
+    spice
+    wget
+    vim
+    nano
+    git
+    git-lfs
+    ripgrep
+    htop
+    file
+    gnumake
+    hyperfine
+    tokei
+    fortune
+    cowsay
+    jq
+    lolcat
+    p7zip
+    mkpasswd
+    gnupg
+    pinentry-all
+    ventoy
+    keepassxc
+
+    firefox
+    chromium
+    thunderbird
+    libreoffice
+    thonny
+    jupyter
+#    rstudio
+    geogebra6
+    arduino
+    greenfoot
+    racket
+    sublime-merge
+    vscode
+    vscode-extensions.mhutchie.git-graph
+    octave
+
+    pandoc
+    nano
+    bat
+    fzf
+    tree
+    navi
+    fzf
+    direnv
+    thefuck
+    ffmpeg
+    cmatrix
+    libsecret
+ 
+    R
+    nodejs
+    rakudo
+    zef
+    zeromq
+    obsidian
+    texliveFull
+    gcc
+    libgcc
+    jdk
+    openssl
+  ];
+
+  #zef install Jupyter::Chatbook --serial
+  #jupyter-chatbook.raku --generate-config
+  #nix profile install .#filius --extra-experimental-features "nix-command flakes"
+  #export NIXPKGS_ALLOW_UNFREE=1; nix profile install .#tigerjython --extra-experimental-features "nix-command flakes" --impure  
+
+  users.defaultUserShell = pkgs.zsh;
+
+  programs = {
+      zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestions.enable = true;
+          syntaxHighlighting.enable = true;
+          ohMyZsh = {
+            enable = true;
+            theme = "robbyrussell";
+            plugins = [
+              "thefuck"
+              "git"
+              "history"
+              "sudo"
+              "copyfile"
+              "copypath"
+              "dirhistory"
+              "jsontools"
+              "web-search"
+              "direnv"
+            ];
+          };
+      };
+  };
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Optimising the NixOS store with automatic options. This will optimise the
+  # store on every build which may slow down builds. The alternativ is to set
+  # them on specific dates like this:
+  # nix.optimise.automatic = true;
+  # nix.optimise.dates = [ "03:45" ]; # Optional; allows customizing schedule
+  nix.settings.auto-optimise-store = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
