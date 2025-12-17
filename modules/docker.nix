f9eca3b4548a9a057d19583cfcc6558f3f0c24e3
rcmlz <rcmlz@github.com>
2025-12-17 15:21:34 +0100

    Simpliy docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,19 +1,4 @@
 { pkgs, lib, ... }:
-let
-    # https://wiki.nixos.org/wiki/Python
-    my-python = pkgs.python3.override {
-      self = my-python;
-      packageOverrides = pyfinal: pyprev: {
-        # dieses Python-Packete exitieren leider nicht in search.nixos.org - deshalb müssen wir sie selbst bauen
-        pedal = pyfinal.callPackage ../nix-shells/extra-packages/pedal.nix { };
-        pgzero = pyfinal.callPackage ../nix-shells/extra-packages/pgzero.nix { };
-        jturtle = pyfinal.callPackage ../nix-shells/extra-packages/jturtle.nix { };
-        jupyterlab-rise = pyfinal.callPackage ../nix-shells/extra-packages/jupyterlab-rise.nix { };
-        jupyterlab-mathjax3 = pyfinal.callPackage ../nix-shells/extra-packages/jupyterlab-mathjax3.nix { };
-        itables = pyfinal.callPackage ../nix-shells/extra-packages/itables.nix { };
-      };
-    };
-in
 {
     virtualisation.docker = {
         # Consider disabling the system wide Docker daemon
