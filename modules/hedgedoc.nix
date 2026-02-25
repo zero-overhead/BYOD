ace60ae53544c0a63271c226da5bc271fa5dcb91
rcmlz <rcmlz@github.com>
2026-02-25 17:20:32 +0100

    Adding hedgedoc

---
+++ modules/hedgedoc.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  # we assume you run in Virtual Box and have NAT from 127.0.0.1:8001 to 10.0.2.15:8001
+  # https://wiki.nixos.org/wiki/Hedgedoc
+  services.hedgedoc = {
+    enable = true;
+    settings = {
+        domain = "127.0.0.1";
+        port = 8001;
+        host = "0.0.0.0"; # Listen to all Interfaces
+        urlAddPort = true;
+        protocolUseSSL = false;
+        allowEmailRegister = false;
+        allowAnonymous = true;
+#        allowOrigin = [
+#          "0.0.0.0"
+#          "127.0.0.1"
+#          "localhost"
+#        ];
+    };
+  };
+}
--- server/vm-headless-no-ollama.nix
+++ server/vm-headless-no-ollama.nix
@@ -5,8 +5,6 @@
       ./basic-setup.nix
       ../modules/jupyterhub.nix
       ../modules/lamp.nix
-      ../modules/ollama.nix
-      ../modules/open-webui.nix
       ../modules/hedgedoc.nix
   ];
   services.getty.helpLine = ''
@@ -14,13 +12,11 @@
   Adminer     http://127.0.0.1:8088/adminer/
   Web-Home    http://127.0.0.1:8088/~demo/
   TinyFileMgr http://127.0.0.1:8088/tinyfilemanager/
-  Open-WebUI  http://127.0.0.1:8080
-  Ollama      http://127.0.0.1:11434
   Hedgedoc    http://127.0.0.1:8001
   
   IP adress  ip iw ifconfig or fastfetch
   System     htop btop nvitop
   Shutdown   sudo shutdown now -h
-  Update     curl -fsSL https://raw.githubusercontent.com/zero-overhead/BYOD/refs/heads/main/setup-server.sh | bash -s server/vm-headless.nix
+  Update     curl -fsSL https://raw.githubusercontent.com/zero-overhead/BYOD/refs/heads/main/setup-server.sh | bash -s server/vm-headless-no-ollama.nix
   '';
 }
